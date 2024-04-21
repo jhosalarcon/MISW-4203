edf588975098e8b1d643c87355901bfbff563dfc
jhosalarcon <ja.alarcon2@uniandes.edu.co>
2024-04-21 14:52:17 -0500

    :sparkles: Show albumes list

--- vinilos_g24/app/src/main/java/com/misw/vinilos_g24/MainActivity.kt
+++ vinilos_g24/app/src/main/java/com/misw/vinilos_g24/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.misw.vinilos_g24
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.misw.vinilos_g24.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -40,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_artistas, R.id.nav_albumes, R.id.nav_premios, R.id.nav_coleccionistas, R.id.nav_retro
+                R.id.nav_home, R.id.nav_artistas, R.id.nav_albumes, R.id.nav_premios, R.id.nav_coleccionistas
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
